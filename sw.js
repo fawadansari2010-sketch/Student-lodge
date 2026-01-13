52f2fd6dd0ba69bba96eca14338ee2646829d98a
fawadansari2010-sketch <fawadansari2010@gmail.com>
2026-01-13 15:20:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const cacheName = 'student-lodge-v4';
+const cacheName = 'student-lodge-v5';
 const assets = [
   '/',
   '/index.html',
   '/manifest.json',
   '/icon.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
 ];
 
 self.addEventListener('install', e => {

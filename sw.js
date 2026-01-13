1892646097f02c0b9d9264e348e6e2fcafdcd094
fawadansari2010-sketch <fawadansari2010@gmail.com>
2026-01-13 14:51:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'student-lodge-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
